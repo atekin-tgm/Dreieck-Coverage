b179cce7e2d9c4d38aff0f46076c7e1b5daf20f0
atekin-tgm <atekin@student.tgm.ac.at>
2016-04-18 15:07:16 +0200

    Ein weiterer Commit-Test

---
+++ test/dreieck/TestIstDreieck.java
@@ -0,0 +1,85 @@
+package dreieck;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+/* 	Testklasse zu Dreieck.java
+ * 	@author		Abdurrahim Burak TEKIN 
+ * 	@version	13.04.2016
+ */
+
+public class TestIstDreieck {
+
+	int seite_a;
+	int seite_b;
+	int seite_c;
+	
+	private Dreieck dreieck;
+
+	@Before
+	public void setUp() {
+		dreieck = new Dreieck(seite_a, seite_b, seite_c);
+	}
+	
+	@Test
+	public void istDreieckSeiteANull() {
+		dreieck.setSeite_a(0);
+		dreieck.setSeite_b(1);
+		dreieck.setSeite_c(2);
+		
+		assertFalse(dreieck.istDreieck());
+	}
+	
+	@Test
+	public void istDreieckSeiteBNull() {
+		dreieck.setSeite_a(1);
+		dreieck.setSeite_b(0);
+		dreieck.setSeite_c(2);
+		
+		assertFalse(dreieck.istDreieck());
+	}
+	
+	@Test
+	public void istDreieckSeiteCNull() {
+		dreieck.setSeite_a(1);
+		dreieck.setSeite_b(2);
+		dreieck.setSeite_c(0);
+		
+		assertFalse(dreieck.istDreieck());
+	}
+	
+	
+	@Test
+	public void istDreieckSeiteAKleinerNull() {
+		dreieck.setSeite_a(0);
+		dreieck.setSeite_b(1);
+		dreieck.setSeite_c(2);
+		
+		assertFalse(dreieck.istDreieck());
+	}
+	
+	@Test
+	public void istDreieckSeiteBKleinerNull() {
+		dreieck.setSeite_a(0);
+		dreieck.setSeite_b(1);
+		dreieck.setSeite_c(2);
+		
+		assertFalse(dreieck.istDreieck());
+	}
+	
+	@Test
+	public void istDreieckSeiteCKleinerNull() {
+		dreieck.setSeite_a(0);
+		dreieck.setSeite_b(1);
+		dreieck.setSeite_c(2);
+		
+		assertFalse(dreieck.istDreieck());
+	}
+	
+	// HALLOOO!! PUSH TEST!
+	
+	// Noch ein Test!
+	
+	// 3. Commit test!
+	
+}
